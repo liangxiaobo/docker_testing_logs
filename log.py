086c84d9a8a12a6ebb8329cdc8f347a158ec2517
梁小波 <liangxiaobo@nahong.com.cn>
2019-05-22 17:19:19 +0800

    修改log.py中的time.sleep(2)

--- log.py
+++ log.py
@@ -9,11 +9,9 @@ fake = Faker('zh_CN')
 logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger("log-test-v1.0.0")
 
-count = 0
 while True:
     # logger.info('Informational message count %d', count)
     logger.info(u'测试数据: ======= %s %s %s', fake.name(), fake.address(), fake.email())
     logger.warning(u'测试数据: ======= %s', fake.company())
     logger.error(u'测试数据: ======= %s %s', fake.job(), fake.phone_number())
-    time.sleep(10)
-    count = count + 1
+    time.sleep(2)
