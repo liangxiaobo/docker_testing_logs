25d0b911301dd00cc0f3c8e05feba322ec74d582
梁小波 <liangxiaobo@nahong.com.cn>
2019-05-22 17:17:31 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM centos
+
+ADD log.py /opt/log.py
+
+RUN yum install -y epel-release
+RUN yum install -y python36
+RUN cd /opt
+RUN curl -O https://bootstrap.pypa.io/get-pip.py
+RUN python3 get-pip.py
+
+RUN pip3 install Faker
+
+# 中文编码
+RUN locale
+RUN localedef -i zh_CN -c -f UTF-8 zh_CN.UTF-8
+RUN echo "export LC_ALL=zh_CN.UTF-8" >> /etc/profile && source /etc/profile
+ENV LANG zh_CN.UTF-8
+ENV LC_CTYPE zh_CN.UTF-8
+
+#RUN python3 log.py
+ENTRYPOINT ["python3", "/opt/log.py"]
---
+++ log.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import logging
+import time
+from faker import Faker
+fake = Faker('zh_CN')
+
+logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger("log-test-v1.0.0")
+
+count = 0
+while True:
+    # logger.info('Informational message count %d', count)
+    logger.info(u'测试数据: ======= %s %s %s', fake.name(), fake.address(), fake.email())
+    logger.warning(u'测试数据: ======= %s', fake.company())
+    logger.error(u'测试数据: ======= %s %s', fake.job(), fake.phone_number())
+    time.sleep(10)
+    count = count + 1
